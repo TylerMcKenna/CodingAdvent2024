e0a8c321454dbc90c36cdfa45d6ad14d9bcc35ef
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2024-12-03 15:19:45 -0600

    Day 3 question 2 completed!

--- Code/DayThree.java
+++ Code/DayThree.java
@@ -9,21 +9,24 @@ public class DayThree {
     public static void main(String[] args) {
         try {
             String file = Files.readString(Paths.get("./DayThree.txt"));
-            Pattern p = Pattern.compile("mul[(][0-9]{1,3},[0-9]{1,3}[)]|don't()|do()");
+            Pattern p = Pattern.compile("mul\\(\\d{1,3},\\d{1,3}\\)|don't|do");
             Matcher m = p.matcher(file);
 
-            ArrayList<String> strings = new ArrayList<>();
-            boolean add = true;
+            ArrayList<String> operations = new ArrayList<>();
+            boolean isDisabled = false;
+            // Leave only operations.add(match) for question one's answer
             while (m.find()) {
-                if (m.group() == "don't") add = false;
-                if (add) strings.add(m.group());
-                if (m.group() == "do") add = true;
+                String match = m.group();
+                if ("don't".equals(match)) 
+                    isDisabled = true;
+                else if ("do".equals(match))
+                    isDisabled = false;
+                else if (!isDisabled) 
+                    operations.add(match);
             }
 
-
-
             int sum = 0;
-            for (String s : strings) {
+            for (String s : operations) {
                 int p1 = s.indexOf("(");
                 int c = s.indexOf(",");
                 int p2 = s.indexOf(")");
@@ -32,7 +35,7 @@ public class DayThree {
                 sum += firstNum * secondNum;
             }
 
-            System.out.println("Q1: " + sum);
+            System.out.println(sum);
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
